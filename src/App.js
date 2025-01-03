f65740fab37ab466bd5975c7736328970a6485b8
Dornavan <130479724+dorndickence@users.noreply.github.com>
2025-01-03 15:49:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import './App.css';
-import Dashboard from './layouts/Dashboard';
-import 'semantic-ui-css/semantic.min.css'
+import React, { useState } from 'react';
+import axios from 'axios';
 
+const App = () => {
+  const [amount, setAmount] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [transactionType, setTransactionType] = useState('deposit');
 
-function App() {
+  const handleTransaction = async () => {
+    try {
+      const response = await axios.post('https://m-pesa-prompt.onrender.com/mpesa', {
+       amount,
+        phoneNumber,
+        transactionType,
+      });
+      console.log('Transaction Success:', response.data);
+    } catch (error) {
+      console.error('Transaction Failed:', error.response.data);
+    }
+  };
+
   return (
-    <div className="App">
-        <Dashboard/>
+    <div>
+      <h2>M-PESA Transaction</h2>
+      <input 
+        type="text" 
+        placeholder="Amount" 
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)} 
+      />
+      <input 
+        type="text" 
+        placeholder="Phone Number" 
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)} 
+      />
+      <select onChange={(e) => setTransactionType(e.target.value)} value={transactionType}>
+        <option value="deposit">Deposit</option>
+        <option value="withdraw">Withdraw</option>
+      </select>
+      <button onClick={handleTransaction}>Submit</button>
     </div>
   );
-}
+};
 
 export default App;
