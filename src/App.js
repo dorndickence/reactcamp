7d7e020a48c7faf460b8941c424e9fa611ee5601
Dornavan <130479724+dorndickence@users.noreply.github.com>
2025-01-03 16:16:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import "./App.css";
 const App = () => {
   const [amount, setAmount] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
